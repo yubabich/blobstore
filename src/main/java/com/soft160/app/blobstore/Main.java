eea39de9fbcdd317b022720dd568ae06cab2c287
Yuri Babich <yuri@babichs.com>
2016-10-07 23:54:42 -0700

    Add files

---
+++ src/main/java/com/soft160/app/blobstore/Main.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.soft160.app.blobstore;
+
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author yuri
+ */
+public class Main
+{
+    private static final Logger LOGGER = Logger.getLogger("main");
+    
+    public static void main(String[] args)
+    {
+        LOGGER.info("Start");
+        System.out.println("Hello world!");
+    }
+}
